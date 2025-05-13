6cbd96c2fa818302f74eb3fcb49fbc26956de701
sagihasan <sagihasan14@gmail.com>
2025-05-13 11:33:29 +0300

    Update macro.py

--- macro.py
+++ macro.py
@@ -2,6 +2,7 @@
 
 from fundamentals import analyze_fundamentals
 from technicals import run_technical_analysis
+from stock_list import STOCK_LIST
 from utils import send_discord_message
 from config import DISCORD_PUBLIC_WEBHOOK
 from collections import defaultdict
