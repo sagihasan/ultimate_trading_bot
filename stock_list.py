f3ec4154687b1ed2e9b2514631d2dc057749380e
sagihasan <sagihasan14@gmail.com>
2025-05-13 11:28:24 +0300

    Create stock_list.py

---
+++ stock_list.py
@@ -0,0 +1,10 @@
+# stock_list.py
+# רשימת המניות לסריקה יומית
+
+STOCK_LIST = [
+    "PLTR", "AMZN", "NVDA", "AAPL", "TSLA", "ANET", "SNEX", "CRGY",
+    "MSFT", "GOOG", "AMD", "ADBE", "META", "AI", "AR", "ALSN", "ASGN",
+    "HIMS", "ASTS", "HOOD", "DKNG", "SOUN", "APP", "PZZA", "AVGO", "SMCI",
+    "ADI", "SEDG", "ARKK", "PERI", "NU", "ACHC", "SMMT", "ZIM", "GRPN",
+    "RKT", "EBAY", "CVNA", "XBI", "PANW", "NFLX"
+]
