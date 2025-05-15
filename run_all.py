42aa6e0fc857207342329730eb021b688111cbfa
sagihasan <sagihasan14@gmail.com>
2025-05-15 10:15:22 +0300

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -1,21 +1,16 @@
-# run_all.py
-
 from main import main
 from scheduler import run_scheduler
-from utils import get_today_events, mark_holiday_message_sent, already_sent_holiday_message
 from changelog_manager import send_latest_changelog
 import threading
+import time
 
 if __name__ == "__main__":
-    # שליחת העדכון האחרון מה־CHANGELOG
-    send_latest_changelog()
+    send_latest_changelog()  # שליחת יומן עדכונים לערוץ הפרטי
 
-    # הפעלת ניהול עסקאות ודוחות ברקע
     scheduler_thread = threading.Thread(target=run_scheduler)
     scheduler_thread.start()
 
-    # הרצת הבוט הראשי של האיתות
+    time.sleep(2)
     main()
 
-    # המתנה לסיום הסקדולר (אופציונלי)
     scheduler_thread.join()
