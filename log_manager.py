7d5a4098de80c28a2b869dc9250f828852bb3d53
sagihasan <sagihasan14@gmail.com>
2025-05-15 12:58:26 +0300

    Update log_manager.py

--- log_manager.py
+++ log_manager.py
@@ -1,1 +1,8 @@
+from datetime import datetime
+from discord_manager import send_error_message
 
+def log_error(error_message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    full_message = f"[{timestamp}] שגיאה: {error_message}"
+    print(full_message)
+    send_error_message(full_message)
