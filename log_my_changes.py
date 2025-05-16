52394dc64298be40a0649a93fcc800e3b3715577
sagihasan <sagihasan14@gmail.com>
2025-05-16 20:25:08 +0300

    Create log_my_changes.py

---
+++ log_my_changes.py
@@ -0,0 +1,8 @@
+from changelog_manager import log_update
+
+log_update(
+    title="שילוב ניתוח גאפ אוטומטי",
+    description="הבוט מזהה גאפ צפוי לפי אפטר/פרה מרקט ומשגר התרעה קרבית.",
+    start_date="2025-05-25",
+    end_date="2025-05-27"
+)
