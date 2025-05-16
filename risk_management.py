3cb26eaf56277b52884ff5cb5cf225249c74594a
sagihasan <sagihasan14@gmail.com>
2025-05-16 15:56:50 +0300

    Update risk_management.py

--- risk_management.py
+++ risk_management.py
@@ -1,13 +1,11 @@
+# risk_management.py
+
 from config import DEFAULT_STOP_LOSS_PERCENT, DEFAULT_TAKE_PROFIT_PERCENT
 
-def calculate_stop_loss(entry_price, direction):
-    if direction == "long":
-        return round(entry_price * (1 - DEFAULT_STOP_LOSS_PERCENT / 100), 2)
-    elif direction == "short":
-        return round(entry_price * (1 + DEFAULT_STOP_LOSS_PERCENT / 100), 2)
+def calculate_stop_loss(entry_price):
+    stop_loss = entry_price * (1 - DEFAULT_STOP_LOSS_PERCENT / 100)
+    return round(stop_loss, 2)
 
-def calculate_take_profit(entry_price, direction):
-    if direction == "long":
-        return round(entry_price * (1 + DEFAULT_TAKE_PROFIT_PERCENT / 100), 2)
-    elif direction == "short":
-        return round(entry_price * (1 - DEFAULT_TAKE_PROFIT_PERCENT / 100), 2)
+def calculate_take_profit(entry_price):
+    take_profit = entry_price * (1 + DEFAULT_TAKE_PROFIT_PERCENT / 100)
+    return round(take_profit, 2)
