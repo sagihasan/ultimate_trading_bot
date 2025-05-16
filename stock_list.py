3766ee93d07379e575bff7e63b23e218b28ea8e5
sagihasan <sagihasan14@gmail.com>
2025-05-16 16:02:49 +0300

    Update stock_list.py

--- stock_list.py
+++ stock_list.py
@@ -1,18 +1,17 @@
 # stock_list.py
-# רשימת המניות לסריקה יומית
 
-STOCKS = [
- "AAPL", "MSFT", "NVDA", "GOOG", "AMZN", "META", "TSLA", "AMD", "ADBE", "AVGO",
-    "NFLX", "INTC", "CRM", "ORCL", "QCOM", "CSCO", "SHOP", "SNOW", "PLTR", "UBER",
-    "BABA", "TCEHY", "LRCX", "ASML", "MU", "TXN", "PANW", "SMCI", "SEDG", "ENPH",
-    "ZM", "DOCU", "SQ", "PYPL", "COIN", "SOFI", "ROKU", "DDOG", "NET", "ZS",
-    "CROX", "DKNG", "HOOD", "HIMS", "PERI", "APP", "AI", "SOUN", "ANET", "SNEX",
-    "CRGY", "ARKK", "NU", "ACHC", "SMMT", "ZIM", "GRPN", "RKT", "EBAY", "CVNA",
-    "XBI", "PZZA", "ALSN", "AR", "ASGN", "ASTS", "ADI", "TEAM", "FSLR", "RUN",
-    "BLNK", "LCID", "RIVN", "NIO", "XPEV", "LI", "CHPT", "TTD", "TTWO", "ATVI",
-    "EA", "INTUIT", "ABNB", "LYFT", "WBD", "WMT", "COST", "TGT", "LOW", "HD",
-    "SBUX", "MCD", "PEP", "KO", "JNJ", "MRK", "PFE", "CVX", "XOM", "APA",
-    "FANG", "OXY", "SLB", "HAL", "WFC", "BAC", "C", "JPM", "GS", "MS",
-    "BX", "SCHW", "V", "MA", "AXP", "TROW", "BK", "AMP", "SPY", "QQQ",
-    "DIA", "IWM", "XLK", "XLF", "XLE", "XLY", "XLV", "XLP"
+STOCK_LIST = [
+‏ "AAPL", "MSFT", "NVDA", "GOOG", "AMZN", "META", "TSLA", "AMD", "ADBE", "AVGO",
+‏    "NFLX", "INTC", "CRM", "ORCL", "QCOM", "CSCO", "SHOP", "SNOW", "PLTR", "UBER",
+‏    "BABA", "TCEHY", "LRCX", "ASML", "MU", "TXN", "PANW", "SMCI", "SEDG", "ENPH",
+‏    "ZM", "DOCU", "SQ", "PYPL", "COIN", "SOFI", "ROKU", "DDOG", "NET", "ZS",
+‏    "CROX", "DKNG", "HOOD", "HIMS", "PERI", "APP", "AI", "SOUN", "ANET", "SNEX",
+‏    "CRGY", "ARKK", "NU", "ACHC", "SMMT", "ZIM", "GRPN", "RKT", "EBAY", "CVNA",
+‏    "XBI", "PZZA", "ALSN", "AR", "ASGN", "ASTS", "ADI", "TEAM", "FSLR", "RUN",
+‏    "BLNK", "LCID", "RIVN", "NIO", "XPEV", "LI", "CHPT", "TTD", "TTWO", "ATVI",
+‏    "EA", "INTUIT", "ABNB", "LYFT", "WBD", "WMT", "COST", "TGT", "LOW", "HD",
+‏    "SBUX", "MCD", "PEP", "KO", "JNJ", "MRK", "PFE", "CVX", "XOM", "APA",
+‏    "FANG", "OXY", "SLB", "HAL", "WFC", "BAC", "C", "JPM", "GS", "MS",
+‏    "BX", "SCHW", "V", "MA", "AXP", "TROW", "BK", "AMP", "SPY", "QQQ",
+‏    "DIA", "IWM", "XLK", "XLF", "XLE", "XLY", "XLV", "XLP"
 ]
