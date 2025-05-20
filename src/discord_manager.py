ae798fe9798391d0db62f20b53cf691debb99566
sagihasan <sagihasan14@gmail.com>
2025-05-20 23:18:54 +0300

    Update discord_manager.py

--- src/discord_manager.py
+++ src/discord_manager.py
@@ -1,6 +1,9 @@
 import os
 from utils import send_message_with_delay
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # ייבוא קבועים מתוך .env
 DISCORD_PRIVATE_WEBHOOK_URL = os.getenv("DISCORD_PRIVATE_WEBHOOK")
