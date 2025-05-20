3c51bb4140d32184dbe5be4fbe9d1b0597dc6fd3
sagihasan <sagihasan14@gmail.com>
2025-05-20 11:12:04 +0300

    Update ml_model.py

--- src/ml_model.py
+++ src/ml_model.py
@@ -14,7 +14,7 @@ def train_model():
     try:
         data = pd.read_csv(DATA_PATH)
         X = data.drop(columns=["result"])
-        y = data["result"]
+        y = data["success"]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
