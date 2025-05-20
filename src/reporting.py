ee2a1138ad699aacdd950793d87d883bb99d85b4
sagihasan <sagihasan14@gmail.com>
2025-05-20 10:23:35 +0300

    Update reporting.py

--- src/reporting.py
+++ src/reporting.py
@@ -3,23 +3,29 @@
 from report_generator import send_monthly_report_to_discord
 from macro_analyzer import analyze_macro_trends
 from trade_manager import analyze_open_trades
+from discord_manager import send_private_message
 from datetime import datetime
 
 def send_monthly_report_if_needed():
     if datetime.today().day == 1:
-        print("שליחת דוח חודשי")
+        print("שליחה זה הדוח החודשי")
         send_monthly_full_report()
 
 def send_weekly_report():
-    # שליחת דוח שבועי בימי שבת בשעה 12:00 בצהריים
-    print("שליחת דוח שבועי")
-
+    # שליחת דוח שבועי ביום שבת בשעה 12:00 בצהריים
+    print("שליחה זה דוח שבועי")
     send_weekly_full_report()
 
 def send_weekly_full_report():
-    analyze_macro_trends()
-    analyze_open_trades()
-    print("Weekly report sent successfully.")
+    macro = analyze_macro_trends()
+    trades = analyze_open_trades()
+    
+    message = "**דוח שבועי - ניתוחים ועדכונים:**\n"
+    message += f"{macro}\n\n"
+    message += f"{trades}"
+    
+    send_private_message(message)
+    print("Weekly report sent.")
 
 def send_monthly_full_report():
     print("Generating full monthly report...")
