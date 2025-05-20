ad488619a3932224a366a6a7d8fb82050b9600f1
sagihasan <sagihasan14@gmail.com>
2025-05-20 10:52:12 +0300

    Update run_all.py

--- src/run_all.py
+++ src/run_all.py
@@ -1,7 +1,7 @@
 # run_all.py
 
 from main import run_bot
-from reporting import send_monthly_report_if_needed, send_weekly_report
+from reporting import send_monthly_report_if_needed, send_weekly_report_if_needed
 from keep_alive import keep_alive
 from scheduler import start_scheduler
 from monthly_planner import send_monthly_plan
