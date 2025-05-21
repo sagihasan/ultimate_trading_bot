450c75c0ab8840913479edd023dccb0fafc587c8
sagihasan <sagihasan14@gmail.com>
2025-05-21 22:43:20 +0300

    Delete src directory

--- src/messaging.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def send_message(webhook_url, message):
-    try:
-        data = {"content": message}
-        response = requests.post(webhook_url, json=data)
-        response.raise_for_status()
-    except Exception as e:
-        print(f"שגיאה בשליחת הודעה לדיסקורד: {e}")
