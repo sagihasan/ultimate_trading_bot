149d4f480de547f018cfe1df1c62afbd34edece4
sagihasan <sagihasan14@gmail.com>
2025-05-22 13:43:01 +0300

    Create scheduler_test.py

---
+++ src/scheduler_test.py
@@ -0,0 +1,14 @@
+from src.discord_manager import send_private_message
+from datetime import datetime
+
+def simulate_start_and_end():
+    now = datetime.now()
+    start_message = f"הבוט התחיל לפעול - {now.strftime('%Y-%m-%d %H:%M')}"
+    end_message = f"הבוט סיים את הפעולה - {now.strftime('%Y-%m-%d %H:%M')}"
+
+    # שליחת ההודעות לדיסקורד לבדיקה
+    send_private_message(start_message)
+    send_private_message(end_message)
+
+if __name__ == "__main__":
+    simulate_start_and_end()
