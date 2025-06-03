3ea34c278259b2979a0ab71c337798045883880b
sagihasan <sagihasan14@gmail.com>
2025-06-03 18:43:35 +0300

    Delete src directory

--- src/check_time.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import pytz
-
-# זמן ברירת מחדל (UTC)
-print("UTC:", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
-
-# זמן ישראל
-israel_time = datetime.now(pytz.timezone('Asia/Jerusalem'))
-print("שעון ישראל:", israel_time.strftime('%Y-%m-%d %H:%M:%S'))
