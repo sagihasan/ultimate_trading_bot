4d1d16751434a48cbc678a2928d710ad0237a78e
sagihasan <sagihasan14@gmail.com>
2025-06-05 19:13:02 +0300

    Create time_config.py

---
+++ src/time_config.py
@@ -0,0 +1,11 @@
+# הגדרות שעות קבועות לכל המערכת
+
+# שעות התחלה וסיום
+START_HOUR = 11
+START_MINUTE = 0
+END_HOUR = 2
+END_MINUTE = 10
+
+# שעה של אירוע מאקרו (למשל נאום הפד)
+MACRO_EVENT_HOUR = 17
+MACRO_EVENT_MINUTE = 0
